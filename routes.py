0a324fac668b48ed7fc3d77b11e9fb8a8c1b10aa
timjozi911 <145546553+timjozi911@users.noreply.github.com>
2023-11-20 19:10:08 +0200

    refactor: update routes to connect to db

--- routes.py
+++ routes.py
@@ -1,4 +1,5 @@
-from models import Todo
+from typing import List
+from models import Todos
 from fastapi import APIRouter
 
 router = APIRouter()
@@ -9,25 +10,26 @@ def read_root():
 
 # get todos
 @router.get("/todos")
-def get_all_todos():
-    pass
+async def get_all_todos():
+    todos = await Todos.find_all().to_list()
+    return todos
 
 # fetch a todo
 @router.get("/todos/{todo_id}")
-def get_todo(todo_id: int):
-    pass
+def get_todo(todo_id: int) -> List[Todos]:
+    return {"data": "fetch a todo"}
 
 # create todo
 @router.post("/todos")
-def create_todo(todo: Todo):
-    pass
+def create_todo(todo: Todos):
+     return {"data": "create todo"}
 
 # update todo
 @router.put("/todos/{todo_id}")
-def update_todo(todo_id: int, todo_new: Todo):
-    pass
+def update_todo(todo_id: int, todo_new: Todos):
+     return {"data": "update todo"}
 
 # delete todo
 @router.delete("/todos/{todo_id}")
 def delete_todo(todo_id: int):
-    pass
+     return {"data": "delete todo"}
