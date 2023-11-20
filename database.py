8043d1f94b7681d6079c19db67462538e0eb0ae9
timjozi911 <145546553+timjozi911@users.noreply.github.com>
2023-11-20 21:00:03 +0200

    chore: update to .env file

--- database.py
+++ database.py
@@ -1,10 +1,12 @@
+import os
 import beanie
 import motor
 import motor.motor_asyncio
 from models import Todos
-
+from dotenv import load_dotenv
 
 async def init_db():
-    client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://localhost:27017")
+    load_dotenv()
+    client = motor.motor_asyncio.AsyncIOMotorClient(os.getenv('MONGODB_URI'))
 
     await beanie.init_beanie(database=client.db_name, document_models=[Todos])
