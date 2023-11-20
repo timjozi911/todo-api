1b7035b4e5516d3a808c4b4feb5158e54d01d96f
timjozi911 <145546553+timjozi911@users.noreply.github.com>
2023-11-20 19:09:04 +0200

    feat: clean up main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 from routes import router
+from database import init_db
 
 app = FastAPI()
+
+@app.on_event("startup")
+async def connect():
+    await init_db()
 
 app.include_router(router)
