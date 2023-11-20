260dc5ff0bb5d0845a001a11f46c3dc496d6001b
timjozi911 <145546553+timjozi911@users.noreply.github.com>
2023-11-20 19:09:31 +0200

    refactor: updated todo model

--- models.py
+++ models.py
@@ -1,6 +1,18 @@
-from pydantic import BaseModel
+from beanie import Document
+from pydantic import Field
+from datetime import datetime
 
-class Todo(BaseModel):
-    id: int
-    task: str
+class Todos(Document):
+    todo: str = Field(max_length=400)
     isDone: bool
+    date_created: datetime
+
+    class Settings:
+        name = 'todo_db'
+
+    class Config:
+        json_schema_extra = {
+            "todo": "A sample todo",
+            "isDone": "False",
+            "date_created": datetime.now(),
+        }
